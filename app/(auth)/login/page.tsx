9d5bbcf26a148cad002184d3cb8d407e6f7d7d97
Saurav <maharjansaurav2002@gmail.com>
2026-01-19 01:10:28 +0545

    lib made

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -4,7 +4,7 @@ import LoginForm from "../_components/LoginForm";
 
 export default function LoginPage() {
   return (
-    <div className="min-h-screen flex items-center justify-center bg-gradient-to-br from-rose-50 via-pink-100 to-red-100 px-4">
+    <div className="min-h-screen flex items-center justify-center bg-linear-gradient-to-br from-rose-50 via-pink-100 to-red-100 px-4">
       <div className="w-full max-w-md rounded-2xl bg-white p-8 shadow-xl">
         <LoginForm />
       </div>
