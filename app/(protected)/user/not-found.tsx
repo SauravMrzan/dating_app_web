2d770f2d56c5ef750fade065694d98f001dbaae6
Saurav <maharjansaurav2002@gmail.com>
2026-02-28 19:33:44 +0545

    Functional page made

--- app/(protected)/user/not-found.tsx
+++
@@ -1,8 +0,0 @@
-export default function notFound() {
-    return (
-        <div>
-            <h2>404-User Not Found</h2>
-            <p>The user you are looking for does not exist.</p>
-        </div>
-    );
-}
